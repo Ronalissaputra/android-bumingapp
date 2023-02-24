aecb422ff2e7a11851d1c4513477cb541402658d
Ronalis Saputra <macbookair@MacBook-Air-Macbook-2.local>
2023-02-25 03:21:11 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>App</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 'auto',
+  },
+  text: {
+    fontSize: 40,
+  },
+});
